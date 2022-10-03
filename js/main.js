699468b898671d426d7ee50b2d5b695ebcf4b2bd
nastyabess <77615212+nastyabess@users.noreply.github.com>
2022-10-03 12:16:51 +0300

    делает правки по 1 дз

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-function getRandomIntInclusive(min, max) {
+const getRandomIntInclusive = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
 
@@ -7,6 +7,6 @@ function getRandomIntInclusive(min, max) {
   }
 
   return Math.floor(Math.random() * (max - min + 1) + min); // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-}
+};
 
 getRandomIntInclusive();
