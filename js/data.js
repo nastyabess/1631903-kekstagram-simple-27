1f364279582a67548d206da5fc9da54f3e6ffe6e
nastyabess <77615212+nastyabess@users.noreply.github.com>
2022-10-19 09:52:04 +0500

    делает правки по 5 заданию

--- js/data.js
+++ js/data.js
@@ -1,14 +1,14 @@
-import {getRandomArrayElement, getRandomPositiveInteger} from './util.js';
-import {DESCRIPTION_PHOTO, SIMILAR_PHOTO_COUNT} from './setup.js';
+import {getRandomArrayElement, getRandomPositiveInteger} from './utils.js';
+import {DESCRIPTION_PHOTO, SIMILAR_PHOTO_COUNT} from './mock.js';
 
 const createPost = (index) => ({
-  id: index,
-  url: `photos/${index}.jpg`,
+  id: index + 1,
+  url: `photos/${index + 1}.jpg`,
   description: getRandomArrayElement(DESCRIPTION_PHOTO),
   likes: getRandomPositiveInteger(15, 200),
   comments: getRandomPositiveInteger(0, 200),
 });
 
-const createPosts = () => Array.from({length: SIMILAR_PHOTO_COUNT}, (_, index) => createPost(index + 1));
+const createPosts = () => Array.from({length: SIMILAR_PHOTO_COUNT}, (_, index) => createPost(index));
 
 export {createPosts};
