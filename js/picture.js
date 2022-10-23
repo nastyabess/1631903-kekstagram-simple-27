72d18acd5f4cd36810f190a29222eb14ae49a741
nastyabess <77615212+nastyabess@users.noreply.github.com>
2022-10-23 21:51:01 +0500

    делает правки в задании

--- js/picture.js
+++ js/picture.js
@@ -10,11 +10,10 @@ const similarListFragment = document.createDocumentFragment();
 
 similarPosts.forEach(({url, comments, likes}) => {
   const postElement = similarPostsTemplate.cloneNode(true);
-  postElement.querySelector('.picture__img').scr = url;
+  postElement.querySelector('.picture__img').src = url;
   postElement.querySelector('.picture__comments').textContent = comments;
   postElement.querySelector('.picture__likes').textContent = likes;
   similarListFragment.append(postElement);
 });
 
-similarPostsTemplate.append(similarListFragment);
-
+document.querySelector('.pictures').append(similarListFragment);
